696392fe7ad954546c1899c136aec4d8bf6973e1
jiachien1206 <nini19961206@gmail.com>
2023-04-15 17:34:02 +0800

    feat: create write and edit posts page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import Home from './pages/Home/Home';
+import CreatePost from './pages/CreateEditPost/CreatePost';
+import EditPost from './pages/CreateEditPost/EditPost';
 import Signup from './pages/User/Signup/Signup';
 import Signin from './pages/User/Signin/Signin';
 import Footstep from './pages/User/Footstep/Footstep';
@@ -14,6 +16,8 @@ root.render(
         <Routes>
             <Route path="/" element={<App />}>
                 <Route index element={<Home />} />
+                <Route path="edit/:id" element={<EditPost />} />
+                <Route path="create" element={<CreatePost />} />
                 <Route path="user/signup" element={<Signup />} />
                 <Route path="user/signin" element={<Signin />} />
                 <Route path="user/footstep" element={<Footstep />} />
