d4dac8debd262b20f5c697daf32f952b3ea26f22
jiachien1206 <nini19961206@gmail.com>
2023-04-15 17:29:24 +0800

    feat: add home page to router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import App from './App';
-
+import Home from './pages/Home/Home';
 import Signup from './pages/User/Signup/Signup';
 import Signin from './pages/User/Signin/Signin';
 import Footstep from './pages/User/Footstep/Footstep';
@@ -12,10 +12,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />}></Route>
-            <Route path="user/signup" element={<Signup />} />
-            <Route path="user/signin" element={<Signin />} />
-            <Route path="user/footstep" element={<Footstep />} />
+            <Route path="/" element={<App />}>
+                <Route index element={<Home />} />
+                <Route path="user/signup" element={<Signup />} />
+                <Route path="user/signin" element={<Signin />} />
+                <Route path="user/footstep" element={<Footstep />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Route>
         </Routes>
     </BrowserRouter>
 );
