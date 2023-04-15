891b2c43637b3d5cbdadcb3563930ae4618b7bb6
jiachien1206 <nini19961206@gmail.com>
2023-04-15 17:27:36 +0800

    feat: add user pages to router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 
+import Signup from './pages/User/Signup/Signup';
+import Signin from './pages/User/Signin/Signin';
+import Footstep from './pages/User/Footstep/Footstep';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />}></Route>
+            <Route path="user/signup" element={<Signup />} />
+            <Route path="user/signin" element={<Signin />} />
+            <Route path="user/footstep" element={<Footstep />} />
         </Routes>
     </BrowserRouter>
 );
