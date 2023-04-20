c7b3c55c8cd8ea66df78b54fd7afadb82a0c36b3
jiachien1206 <nini19961206@gmail.com>
2023-04-20 13:05:37 +0800

    feat: create MsSave Page

---
+++ src/pages/User/MySave/SavedPosts.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+import api from '../../../utils/api.js';
+
+const Save = () => {
+    const [posts, setPosts] = React.useState([]);
+    return <h1>save</h1>;
+};
+
+export default Save;
