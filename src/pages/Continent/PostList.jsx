748a9af885e54a4643b59ec7034917fabc0a78e2
jiachien1206 <nini19961206@gmail.com>
2023-04-23 10:22:48 +0800

    fix: sync continent pages like and save status

--- src/pages/Continent/PostList.jsx
+++ src/pages/Continent/PostList.jsx
@@ -8,10 +8,27 @@ const PostsWrap = styled.div`
 `;
 
 const PostList = ({ posts }) => {
+    const [likedPosts, setLikedPosts] = React.useState([]);
+    const [savedPosts, setSavedPosts] = React.useState([]);
+
+    const getLikedPosts = () => {
+        const posts = window.localStorage.getItem('likedPosts');
+        setLikedPosts(JSON.parse(posts));
+    };
+
+    const getSavedPosts = () => {
+        const posts = window.localStorage.getItem('savedPosts');
+        setSavedPosts(JSON.parse(posts));
+    };
+    React.useEffect(() => {
+        getLikedPosts();
+        getSavedPosts();
+    }, [posts]);
+
     return (
         <PostsWrap>
             {posts.map((post) => (
-                <Post key={post._id} post={post} />
+                <Post key={post._id} post={post} likedPosts={likedPosts} savedPosts={savedPosts} />
             ))}
         </PostsWrap>
     );
