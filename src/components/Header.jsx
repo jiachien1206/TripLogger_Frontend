bacbcf387c1996c941da86b5a4c62b2300c585e1
jiachien1206 <nini19961206@gmail.com>
2023-04-30 10:57:56 +0800

    chore: change url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -214,12 +214,12 @@ const Header = () => {
 
     const [ws, setWs] = React.useState(null);
     React.useEffect(() => {
-        const newWs = webSocket('http://localhost:8000');
+        const newWs = webSocket('https://chiaproject.com');
         setWs(newWs);
     }, []);
     React.useEffect(() => {
         if (isLogin) {
-            const newWs = webSocket('http://localhost:8000');
+            const newWs = webSocket('https://chiaproject.com');
             setWs(newWs);
         }
         if (isLogin) {
