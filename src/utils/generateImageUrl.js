3d8ba4d4fec72f257a15173d78f15b9ef1b381e5
jiachien1206 <nini19961206@gmail.com>
2023-06-30 02:18:34 +0800

    hotfix: unsplash free photos can't be accessed

--- src/utils/generateImageUrl.js
+++ src/utils/generateImageUrl.js
@@ -1,8 +1,12 @@
 export const imageUrl = (url) => {
     if (url) {
-        return url.replace(
-            'https://triplogger.s3.ap-northeast-1.amazonaws.com',
-            'https://demj1sg6qvpz9.cloudfront.net'
-        );
+        if (url.includes('source')) {
+            return 'https://demj1sg6qvpz9.cloudfront.net/Maldives/main-s.jpg';
+        } else {
+            return url.replace(
+                'https://triplogger.s3.ap-northeast-1.amazonaws.com',
+                'https://demj1sg6qvpz9.cloudfront.net'
+            );
+        }
     }
 };
